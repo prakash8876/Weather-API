240519cfcb8c4e54417ed0ad03c7cb7851637863
prakash8876 <42605640+prakash8876@users.noreply.github.com>
2020-10-20 11:50:19 +0530

    deleted service
    Deleted unwanted packages

--- weather-api/src/main/java/com/example/web/weatherapi/service/WeatherService.java
+++
@@ -1,9 +0,0 @@
-package com.example.web.weatherapi.service;
-
-import com.example.web.weatherapi.model.WeatherModel;
-
-public interface WeatherService {
-
-	public WeatherModel getWeather(long lat, long lon);
-	
-}
